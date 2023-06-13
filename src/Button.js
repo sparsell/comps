985caf8a9cea7d2268280d4fef043365c892eeca
Sonja Parsell <sonjaparsell09@gmail.com>
2023-06-13 09:13:16 -0400

    added prop type to button and validation

--- src/Button.js
+++ src/Button.js
@@ -24,4 +24,17 @@ function Button({
     return <button>{children}</button>
 }
 
+Button.propTypes = {
+    checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+        const count = Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!warning)
+        + Number (!!success)
+        + Number(!!danger)
+        if (count > 1) {
+            return new Error("Only one of primary, secondary, success, warning, danger can be true")
+        }
+    },
+};
+
 export default Button;
