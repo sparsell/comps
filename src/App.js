1234dc0e2a2cfb85a374b785df4c9cbb3c39647e
Sonja Parsell <sonjaparsell09@gmail.com>
2023-06-17 07:10:21 -0400

    moves button to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Button from './Button'
+import Button from './components/Button'
 import { GoCloudDownload, GoDatabase } from 'react-icons/go';
 import { BsCurrencyDollar } from 'react-icons/bs';
 
