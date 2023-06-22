8a6c0eb0b0cb647a3c3782389f36d71dac31b831
Sonja Parsell <sonjaparsell09@gmail.com>
2023-06-22 09:32:25 -0400

    adds Accordion component and functionality

---
+++ src/components/Accordion.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const handleClick =  (nextIndex) => {
+        setExpandedIndex(nextIndex)
+    }; 
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+    const icon = <span>
+        {isExpanded ? 'DOWN' : 'LEFT'}
+        </span>
+
+        return (
+            <div key={item.id}>
+                <div onClick={() => handleClick(index)}>
+                    {icon}{item.label}
+
+                    {isExpanded && <div>{item.content}</div>}
+                </div>
+            </div>
+        );
+    });
+
+    return (
+    <div>{renderedItems}</div>
+    );
+
+};
+
+export default Accordion;
